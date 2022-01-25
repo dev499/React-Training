aae611d76006a61fb740115302da918bda196c6c
dev_agarwal <dev@nssplindia.com>
2022-01-25 10:58:38 +0530

    Day4

---
+++ Day4/Map.js
@@ -0,0 +1,14 @@
+let mp=new Map()
+     
+    // Adding values to the map
+    mp.set("a",1);
+    mp.set("b",2);
+    mp.set("c",3);
+     
+    // Logging map object to console
+    mp.forEach((values,keys)=>{
+      console.log(values,keys)
+    })
+
+
+    
