32bd8f51dc957e761ee04aeb6a1f7bf276109a79
dev_agarwal <dev@nssplindia.com>
2022-01-26 17:28:12 +0530

    login added

--- Day4/BankLogin.js
+++ Day4/BankLogin.js
@@ -1,11 +1,10 @@
 function validation() {
-    var email=document.getElementById('email').value
-    var pwd=document.getElementById('pwd').value
+  var email = document.getElementById("email").value;
+  var pwd = document.getElementById("pwd").value;
 
-    if(email=='admin@gmail.com' && pwd=='admin123'){
-        return true
-    }
-    else  {
-        alert("E-mail or password is incorrect")
-    }
+  if (email == "admin@gmail.com" && pwd == "admin123") {
+    window.location.href = "Bankhome.html";
+  } else {
+    alert("E-mail or password is incorrect");
+  }
 }
