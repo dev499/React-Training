e87bafd97ed5f14fc160989244b6117641562736
dev_agarwal <dev@nssplindia.com>
2022-01-27 12:05:56 +0530

    Node init

---
+++ Node Js/Datatype.js
@@ -0,0 +1,53 @@
+var x = "Welcome to GeeksforGeeks ";
+   
+var y = 'Node.js Tutorials';
+   
+var z = ['Geeks', 'for', 'Geeks'];
+   
+console.log(x);
+   
+console.log(y);
+   
+console.log("Concat Using (+) :", (x + y));
+   
+console.log("Concat Using Function :", (x.concat(y)));
+   
+console.log("Split string: ", x.split(' '));
+   
+console.log("Join string: ", z.join(', '));
+   
+console.log("Char At Index 5: ", x.charAt(5) );
+
+
+
+
+
+// var company = {
+//     Name: "GeeksforGeeks", 
+//     Address: "Noida", 
+//     Contact: "+919876543210",
+//     Email: "abc@geeksforgeeks.org"
+// };
+  
+// // Display the object information
+// console.log("Information of variable company:", company);
+       
+// // Display the type of variable
+// console.log("Type of variable company:", typeof company);
+
+
+// // Variable store number data type
+// var a = 35;
+// console.log(typeof a);
+   
+// // Variable store string data type
+// a = "GeeksforGeeks";
+// console.log(typeof a);
+   
+// // Variable store Boolean data type
+// a = true;
+// console.log(typeof a);
+   
+// // Variable store undefined (no value) data type
+// a = undefined;
+// console.log(typeof a);
